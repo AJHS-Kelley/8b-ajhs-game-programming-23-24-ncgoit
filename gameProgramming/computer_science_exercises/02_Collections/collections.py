4ad17967eaeaedc6751885e272cab13825ffe9f6
Nevaeh <s11944972@students.duvalschools.org>
2023-10-02 14:08:35 -0400

    v0.1b
    Printing List Contents

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,4 +1,4 @@
-# Collections Examples, Nevaeh Copeland, v0.1a
+# Collections Examples, Nevaeh Copeland, v0.1b
 
 # List -- ORDEED, CHANGEABLE, ALLOWS DUPLICATE VALUES
 breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
@@ -8,5 +8,10 @@ breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
 # Python Only: index -1 it is the last item on the list.
 testScores = [95, 100, 25, 15, 27, 35]
 classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
+
+# Printing Contents of an List
+print(breakfastFoods)
+print(testScores)
+print(classGPA)
 
 
