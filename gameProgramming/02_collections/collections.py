e616f65cdd0f5a81c4530b5d99d0edc7f8b0eb90
Nevaeh <s11944972@students.duvalschools.org>
2023-10-04 14:51:53 -0400

    v.01
    Added player inventory code.

---
+++ gameProgramming/02_collections/collections.py
@@ -0,0 +1,10 @@
+playerInventory = []
+while len(playerInventory) < 10:
+    playerInventory.append(input("Add an item to your inventory.\n"))
+playerInventory.sort()
+print(playerInventory)
+
+while len(playerInventory) > 5:
+   playerInventory.remove(input("You can now remove up to five items from your inventory.\n"))
+playerInventory.sort()
+print (playerInventory)
