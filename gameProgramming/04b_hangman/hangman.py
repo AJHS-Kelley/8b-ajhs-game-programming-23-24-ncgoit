5aaee136f2e6f8ee2dc40278914b54fbbb1a5f60
Nevaeh <s11944972@students.duvalschools.org>
2023-10-31 14:15:27 -0400

    v0.2
    added hangman board and while loop

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,3 +1,46 @@
-# Hangman Game by Nevaeh Copeland, v0.1
+# Hangman Game by Nevaeh Copeland, v0.2
 
 words = 'game four five dark moon read eating camera button avenue emerge demand raining absolute mountain sentence children changes trumpet delivery repeated abbreviation television theatre living contemplating jeopardizing naivenesses confused supercalifragilisticexpialidocious'.split()
+
+# VARIABLE_NAMES in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
+HANGMAN_BOARD = ['''
+    +---+
+        |
+        |
+        |
+    =======''', '''
+    +---+
+    O   |
+        |
+        |
+    =======''', '''
+    +---+
+    O   |
+    |   |
+        |
+    =======''', '''
+    +---+
+    O   |
+   /|   |
+        |
+    =======''', '''
+    +---+
+    O   |
+   /|\  |
+        |
+    =======''', '''
+    +---+
+    O   |
+   /|\  |
+   /    |
+    =======''', '''
+    +---+
+    O   |
+   /|\  |
+   / \  |
+    =======''']
+
+i = 0
+while i < len(HANGMAN_BOARD):
+    print(HANGMAN_BOARD[i])
+    i += 1
