7bf52c2b60cba416b548372ec3de140f49e68395
Nevaeh <s11944972@students.duvalschools.org>
2023-11-02 14:03:36 -0400

    v0.5
    Added for and while loops

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,4 +1,4 @@
-# Hangman Game by Nevaeh Copeland, v0.4
+# Hangman Game by Nevaeh Copeland, v0.5
 import random
 words = 'game four five dark moon read eating camera button avenue emerge demand raining absolute mountain sentence children changes trumpet delivery repeated abbreviation television theatre living contemplating jeopardizing naivenesses confused supercalifragilisticexpialidocious'.split()
 
@@ -46,7 +46,7 @@ def getRandomWord(wordList): # Return a random word from the list
     # len(listName) -1 is EXTREMELY COMMON FOR WORKING WITH LISTS.
     return wordList[wordIndex]
 
-def displayBoard (missedLetters, correctLetters, secreatWord):
+def displayBoard (missedLetters, correctLetters, secretWord):
     print(HANGMAN_BOARD[len(missedLetters)])
     print()
 
@@ -54,6 +54,32 @@ def displayBoard (missedLetters, correctLetters, secreatWord):
     for eachLetter in missedLetters:
         print(eachLetter, end = ' ')
         print()
+
+        blanks = '_' * len(secretWord)
+
+    # Replace Blanks with Correct Letters
+    for i in range (len(secretWord)):
+        if secretWord[i] in correctLetters:
+            blanks = blanks [:i] + secretWord[i] + blanks[i+1:]
+
+    for letter in blanks:
+        print(letter, end = ' ')
+    print()
+
+
+def getGuess(alreadyGuessed):
+    while True:
+        print('Please guess a letter and press enter.')
+        guess = input()
+        guess = guess.lower()
+        if len(guess) != 1:
+            print('Please enter a single letter.')
+        elif guess in alreadyGuessed:
+            print('Letter as been guessed already, try again.')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Please guess a LETTER from the English alphabet.')
+        else:
+            return guess 
 
 # i = 0
 # while i < 50:
