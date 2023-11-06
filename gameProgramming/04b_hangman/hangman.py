617cc950dde8d3c98de07c1e6573b7322c33fb56
Nevaeh <s11944972@students.duvalschools.org>
2023-11-06 14:45:04 -0500

    v0.8
    completed version with added difficulty

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,8 +1,15 @@
-# Hangman Game by Nevaeh Copeland, v0.7
+# Hangman Game by Nevaeh Copeland, v0.8
 import random
-words = 'game four five dark moon read eating camera button avenue emerge demand raining absolute mountain sentence children changes trumpet delivery repeated abbreviation television theatre living contemplating jeopardizing naivenesses confused supercalifragilisticexpialidocious'.split()
+# words = 'game four five dark moon read eating camera button avenue emerge demand raining absolute mountain sentence children changes trumpet delivery repeated abbreviation television theatre living contemplating jeopardizing naivenesses confused supercalifragilisticexpialidocious'.split()
+# DICTIONARY VERSION
+# Stored in Key:Value Pairs.
+# Actual Dictionary Word (Key): Value (Definition)
+# Uses {} to specify a dictionary
+words = {'Colors': 'red orange yellow green blue indigo violet fuschia teal garnet gold black white silver gold'.split(),
+         'Animals': 'cat cow dog moose goose fish wombat wolverine giraffe hippoptamus lion alligater'.split(),
+         'Shapes': 'square triangle circle rhombus parallelogram trapezoid diamond dodecahedron'.split(),
+         'Foods': 'hamburger hotdog potato waffle pancake escargot oysters chips steak'.split()}
 
-# VARIABLE_NAMES in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
 HANGMAN_BOARD = ['''
     +---+
         |
@@ -38,13 +45,30 @@ HANGMAN_BOARD = ['''
     O   |
    /|\  |
    / \  |
+    =======''', '''
+    +---+
+    O   |
+  o-|-o |
+   / \  |
+    =======''', '''
+    +---+
+    O   |
+  o-|-o |
+   / \  |
+  o   o |
     =======''']
 
 # Pick Word from list
-def getRandomWord(wordList): # Return a random word from the list
-    wordIndex = random.randint(0, len(wordList) - 1)
-    # len(listName) -1 is EXTREMELY COMMON FOR WORKING WITH LISTS.
-    return wordList[wordIndex]
+# def getRandomWord(wordList): # Return a random word from the list
+#     wordIndex = random.randint(0, len(wordList) - 1)
+#     # len(listName) -1 is EXTREMELY COMMON FOR WORKING WITH LISTS.
+#     return wordList[wordIndex]
+
+# Pick Word from Dictionary
+def getRandomWord(wordDict): # Return a random word from the list
+    wordKey = random.choice(list(wordDict.keys()))
+    wordIndex = random.randint(0,len(wordDict[wordKey]) - 1 )
+    return [wordDict[wordKey][wordIndex], wordKey]
 
 def displayBoard (missedLetters, correctLetters, secretWord):
     print(HANGMAN_BOARD[len(missedLetters)])
@@ -87,13 +111,29 @@ def playAgain():
 
 # Introduce the Game
 print('Welcome to Hangman created by Nevaeh C.')
+
+# CHOOSE DIFFICULTY
+difficulty = 'X'
+while difficulty not in 'EMH':
+    print('Please Choose Easy, Medium or Hard. Type the first letter then press enter.\n')
+    difficulty = input().upper()
+if difficulty == 'M': # MEDIUM
+    del HANGMAN_BOARD[8]
+    del HANGMAN_BOARD[7]
+if difficulty == 'H': # HARD
+    del HANGMAN_BOARD[8]
+    del HANGMAN_BOARD[7]
+    del HANGMAN_BOARD[5]
+    del HANGMAN_BOARD[3]
+
 missedLetters = ''
 correctLetters = ''
-secretWord = getRandomWord(words)
+secretWord, secretSet = getRandomWord(words)
 gameIsDone = False
 
 # Main Game Loop
 while True:
+    print('The scret word is from the ' + secretSet + ' categroy.\n')
     displayBoard(missedLetters, correctLetters, secretWord)
 
     guess = getGuess(missedLetters + correctLetters)
@@ -109,7 +149,7 @@ while True:
                 break
         if foundAllLetters: # if True
             print('Dang you got me :( but W mans!')
-            print('The secret word was' + secretWord)
+            print('The secret word was' +  secretWord)
             gameIsDone = True
     else:
         missedLetters = missedLetters + guess
@@ -125,15 +165,7 @@ while True:
             missedLetters = ''
             correctLetters = ''
             gameIsDone = False
-            secretWord = getRandomWord(words)
+            secretWord, secretSet = getRandomWord(words)
         else:
             break
-
-
-# i = 0
-# while i < 50:
-#     word = getRandomWord(words)
-#     print(word)
-#     i += 1
-
 
