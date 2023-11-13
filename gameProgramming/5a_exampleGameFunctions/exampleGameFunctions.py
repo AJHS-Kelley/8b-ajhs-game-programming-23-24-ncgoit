0f88c65b5b8acc6a445fd80b177a267eb1aa1626
Nevaeh <s11944972@students.duvalschools.org>
2023-11-13 14:46:29 -0500

    v0.2
    setting up function for opposing teams

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,14 +1,49 @@
-# Example Game Functions Project, Nevaeh Copeland, v0.1
+# Example Game Functions Project, Nevaeh Copeland, v0.2
 import random
 
-def functionOne():
+# add List to pick opposing Teams
+
+randomOpposingTeams = [Kansas Jayhawks, Purdue Boilermakers, Arizona Wildcats, Marquette Golden Eagles, UConn Huskies, Houston Cougars, Tennessee Volunteers, Creighton Bluejays, Duke Devils, Florida Atlantic Owls, Gonzaga Bulldogs, Miami Hurricanes, Arkansas Razorbacks, Baylor Bears, South Carolina Trojans, Kentucky Wildcats, Michigan State Spartans, Texas Longhorns, North Carolina Tar Heels, Villanova Wildcats, Alabama Crimson Tide, Illinois Fighting Illini, James Madision Dukes, Colorado Buffaloes,]
+
+# Add jumpball function
+# Add shooting function
+# Add point counter function
+# Add random team for opponent function 
+def getrandomword():
+
     pass
 
-def functionTwo(param1):
+def jumpBall():
     pass
 
-def functionThree(param1 = "Default Value"):
+def shootBall():
     pass
 
-def function(Param1, param2, param3):
+def countPoints():
     pass
+
+
+# def functionOne():
+#     pass
+
+# def functionTwo(param1):
+#     pass
+
+# def functionThree(param1 = "Default Value"):
+#     pass
+
+# def function(Param1, param2, param3):
+#     pass
+
+# def catchBall(throwQuality, passCatchScore, weather):
+#     if throwQuality > 5.0 and passCatchScore >= 90 and weather == 'Sunny':
+#         ballCaught = True
+#     elif throwQuality > 4.0 and passCatchScore >= 75 and weather == 'Windy':
+#         ballCaught = False
+#     else:
+#         print("Oh, no...INTERCEPTION!/n")
+#         ballIntercepted = True
+#         return ballIntercepted
+#     return ballCaught
+
+# catchBall(4.25, 107, 'Rainy')
