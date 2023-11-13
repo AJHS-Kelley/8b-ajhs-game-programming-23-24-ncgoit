321e4fc0520e8a74da2c672c41ca0deb88545bf3
Nevaeh <s11944972@students.duvalschools.org>
2023-11-13 13:51:15 -0500

    v0.1
    preped functions

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,14 @@
-# Example Game Functions Project, Nevaeh Copeland, v0.0
+# Example Game Functions Project, Nevaeh Copeland, v0.1
+import random
+
+def functionOne():
+    pass
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "Default Value"):
+    pass
+
+def function(Param1, param2, param3):
+    pass
