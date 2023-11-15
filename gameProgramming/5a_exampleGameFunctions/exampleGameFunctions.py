a193cf534d6d56527f7c9572208c58d9ef1abc0b
Nevaeh <s11944972@students.duvalschools.org>
2023-11-15 14:32:55 -0500

    v0.3
    updated first function

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,49 +1,25 @@
-# Example Game Functions Project, Nevaeh Copeland, v0.2
+# Example Game Functions Project, Nevaeh Copeland, v0.3
 import random
 
 # add List to pick opposing Teams
 
-randomOpposingTeams = [Kansas Jayhawks, Purdue Boilermakers, Arizona Wildcats, Marquette Golden Eagles, UConn Huskies, Houston Cougars, Tennessee Volunteers, Creighton Bluejays, Duke Devils, Florida Atlantic Owls, Gonzaga Bulldogs, Miami Hurricanes, Arkansas Razorbacks, Baylor Bears, South Carolina Trojans, Kentucky Wildcats, Michigan State Spartans, Texas Longhorns, North Carolina Tar Heels, Villanova Wildcats, Alabama Crimson Tide, Illinois Fighting Illini, James Madision Dukes, Colorado Buffaloes,]
+randomOpposingTeams = ['Kansas Jayhawks, Purdue Boilermakers, Arizona Wildcats, Marquette Golden Eagles, UConn Huskies, Houston Cougars, Tennessee Volunteers, Creighton Bluejays, Duke Devils, Florida Atlantic Owls, Gonzaga Bulldogs, Miami Hurricanes, Arkansas Razorbacks, Baylor Bears, South Carolina Trojans, Kentucky Wildcats, Michigan State Spartans, Texas Longhorns, North Carolina Tar Heels, Villanova Wildcats, Alabama Crimson Tide, Illinois Fighting Illini, James Madision Dukes, Colorado Buffaloes' .split()]
 
 # Add jumpball function
 # Add shooting function
 # Add point counter function
-# Add random team for opponent function 
-def getrandomword():
+# Add random team for opponent function
 
+wordIndex = random.randint(0, len(randomOpposingTeams) - 1)
+
+
+
+
+def getRandomTeam(randomOpposingTeams):
+    opposingTeam = random.randint(0,len(randomOpposingTeams) - 1 )
+    return randomOpposingTeams[opposingTeam]
     pass
 
-def jumpBall():
+def getRandomTeam():
     pass
 
-def shootBall():
-    pass
-
-def countPoints():
-    pass
-
-
-# def functionOne():
-#     pass
-
-# def functionTwo(param1):
-#     pass
-
-# def functionThree(param1 = "Default Value"):
-#     pass
-
-# def function(Param1, param2, param3):
-#     pass
-
-# def catchBall(throwQuality, passCatchScore, weather):
-#     if throwQuality > 5.0 and passCatchScore >= 90 and weather == 'Sunny':
-#         ballCaught = True
-#     elif throwQuality > 4.0 and passCatchScore >= 75 and weather == 'Windy':
-#         ballCaught = False
-#     else:
-#         print("Oh, no...INTERCEPTION!/n")
-#         ballIntercepted = True
-#         return ballIntercepted
-#     return ballCaught
-
-# catchBall(4.25, 107, 'Rainy')
