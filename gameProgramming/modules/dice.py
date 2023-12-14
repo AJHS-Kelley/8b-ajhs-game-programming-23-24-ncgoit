d7f6cf869eebc7ca39d6f9ff1a7d81418508cb42
Nevaeh <s11944972@students.duvalschools.org>
2023-12-14 13:41:05 -0500

    v0.1
    added roll and display function

---
+++ gameProgramming/modules/dice.py
@@ -0,0 +1,23 @@
+# Dice Rolling Module by Nevaeh Copeland, v0.1
+import random
+
+def roll(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        numRolled +=1
+    return sum
+
+def display(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        print(f"Sum: {sum}\n")
+        numRolled +=1
+    return sum
+
