8b52edef45f50965269ede928b3df274ee5ad78c
Nevaeh <s11944972@students.duvalschools.org>
2024-01-11 14:47:54 -0500

    Update dnaGame.py

--- gameProgramming/cohort_lessons/dnaGame.py
+++ gameProgramming/cohort_lessons/dnaGame.py
@@ -1,4 +1,4 @@
-# DNA Replication Game, Ryan Kelly, v0.0
+# DNA Replication Game, Ryan Kelly, v0.1
 
 # Import Entire Modules -- Get the whole tool box.
 import time, datetime
@@ -23,4 +23,19 @@ def genDNA() -> str:
     return dnaSequence
 
 dna = genDNA()
-print(dna)
+
+def genRNA(dnaSquence: str) -> tuple:
+    print(f"The DNA Sequence is a {dnaSquence}.\n")
+    print("You willnow generate the RNA sequence that would match.\n")
+    print("Please Remember, in the RNA sequence U pairs with A from the DNA sequence.\n")
+    rnaStart= time.time() # time.time() returns the number of seconds since 00:00:00 UTC Jan. 01, 1776
+    rnaSequence = input("Please enter the matching RNA sequence. Leave no spaces! Then press enter.\n")
+    rnaStop = time.time()
+    rnaTime = rnaStop - rnaStart
+    return(rnaSequence, rnaTime)
+    # Tuples are ORDERED -- you can reference elements with the index.
+    # Tuples are UNCHANGEABLE -- you cannot add, modify, or delete after creating
+    # Tuples CAN have duplicate values.
+
+rna = genRNA(dna)
+print(rna)
