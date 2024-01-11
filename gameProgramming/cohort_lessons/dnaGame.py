e0da0aa7020880c2629a3134006dccd1c06a0ae2
Nevaeh <s11944972@students.duvalschools.org>
2024-01-11 14:01:52 -0500

    v0.0
    added a module and method

---
+++ gameProgramming/cohort_lessons/dnaGame.py
@@ -0,0 +1,7 @@
+# DNA Replication Game, Ryan Kelly, v0.0
+
+# Import Entire Modules -- Get the whole tool box.
+import time, datetime
+
+# Import Specific Methods -- Get the specific tol.
+from random import choice
