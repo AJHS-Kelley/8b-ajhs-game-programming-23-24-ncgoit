fa4e8018ef54ea0dc515a0fd51ed4354471d925c
Nevaeh <s11944972@students.duvalschools.org>
2024-01-11 14:25:41 -0500

    v0.1
    added function

--- gameProgramming/cohort_lessons/dnaGame.py
+++ gameProgramming/cohort_lessons/dnaGame.py
@@ -5,3 +5,22 @@ import time, datetime
 
 # Import Specific Methods -- Get the specific tol.
 from random import choice
+
+# Store the DNA Bases
+dnaBases = ["A", "T", "G", "C"]
+
+# GAME FUNCTIONS
+def gameIntro() -> None:
+    pass
+
+def genDNA() -> str:
+    basesGenerated = 0
+    basesRequested = int(input("Please enter a position intergar number of bases to generate.\n"))
+    dnaSequence = ""
+    while basesGenerated < basesRequested:
+        dnaSequence += choice(dnaBases)
+        basesGenerated += 1
+    return dnaSequence
+
+dna = genDNA()
+print(dna)
