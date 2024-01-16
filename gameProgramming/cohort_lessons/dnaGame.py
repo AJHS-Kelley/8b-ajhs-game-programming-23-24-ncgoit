58c76de056291e0215753ec11f52912e119ec87e
Nevaeh <s11944972@students.duvalschools.org>
2024-01-16 14:39:36 -0500

    Update dnaGame.py

--- gameProgramming/cohort_lessons/dnaGame.py
+++ gameProgramming/cohort_lessons/dnaGame.py
@@ -27,7 +27,7 @@ dna = genDNA()
 def doTranscription(dnaSquence: str) -> tuple:
     
     print(f"The DNA Sequence is a {dnaSquence}.\n")
-    print("You willnow generate the RNA sequence that would match.\n")
+    print("You will now generate the RNA sequence that would match.\n")
     print("Please Remember, in the RNA sequence U pairs with A from the DNA sequence.\n")
     rnaStart= time.time() # time.time() returns the number of seconds since 00:00:00 UTC Jan. 01, 1776
     rnaSequence = input("Please enter the matching RNA sequence. Leave no spaces! Then press enter.\n").upper()
@@ -56,10 +56,41 @@ def verifySequence(dnaSequence: str, rnaSequence: str) -> bool:
             print ("Unable to identify correct base. No match detected.")
     return isMatch
 
+def calcScore(rnaSequence: str, rnaTime):
+    score = 0
+    if rnaTime < 1.0:
+        score += 1000000
+    elif rnaTime < 5.0:
+        score += 900000
+    elif rnaTime < 15.0:
+        score += 700000
+    elif rnaTime < 25.0:
+        score += 500000
+    else: # Slowest Time, Lowest Score
+        score += 250000
+
+    scoreMulti = 0.0
+    if len(rnaSequence) >= 30: # Longest Sequence, Highest Mulitplier
+        scoreMulti = 5.0
+    elif len(rnaSequence) >= 25:
+        scoreMulti = 4.0
+    elif len(rnaSequence) >= 20:
+        scoreMulti = 3.0
+    elif len(rnaSequence) >= 15:
+        scoreMulti = 2.0
+    elif len(rnaSequence) >= 5:
+        scoreMulti = 1.0
+    else: # Shortest sequence, Lower Multiplier
+        scoreMulti = 0.25
+    # Increase Score, multiplier should be > 1.0
+    # Decrease Score, multiplier should be < 1.0
+    score *= scoreMulti
+    return score
 
 
 dna= genDNA()
 rna = doTranscription(dna)
 print(verifySequence(dna, rna[0]))
 
+print(calcScore(rna[0], rna[1]))
 
