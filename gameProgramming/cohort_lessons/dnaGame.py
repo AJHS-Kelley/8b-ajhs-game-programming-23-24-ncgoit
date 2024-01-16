437aad26e849c0ca1a072054a27aca6b6e829de6
Nevaeh <s11944972@students.duvalschools.org>
2024-01-16 14:45:58 -0500

    Update dnaGame.py

--- gameProgramming/cohort_lessons/dnaGame.py
+++ gameProgramming/cohort_lessons/dnaGame.py
@@ -87,6 +87,13 @@ def calcScore(rnaSequence: str, rnaTime):
     score *= scoreMulti
     return score
 
+def saveScore(dnaSequence: str, rnaSequence: str, rnaTime: float) -> None:
+    playerName = input("What is your first name?\n")
+    lastName = input("What is your last name?\n")
+    fullName = playerName + " " + lastName
+
+    fileName = "dnaReplicationScore" + fullName + ".txt"
+
 
 dna= genDNA()
 rna = doTranscription(dna)
