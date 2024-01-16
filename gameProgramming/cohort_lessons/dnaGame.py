80c719c9fea855e2e53dd8c23eac11cbd9201333
Nevaeh <s11944972@students.duvalschools.org>
2024-01-16 14:15:22 -0500

    Update dnaGame.py

--- gameProgramming/cohort_lessons/dnaGame.py
+++ gameProgramming/cohort_lessons/dnaGame.py
@@ -24,12 +24,13 @@ def genDNA() -> str:
 
 dna = genDNA()
 
-def genRNA(dnaSquence: str) -> tuple:
+def doTranscription(dnaSquence: str) -> tuple:
+    
     print(f"The DNA Sequence is a {dnaSquence}.\n")
     print("You willnow generate the RNA sequence that would match.\n")
     print("Please Remember, in the RNA sequence U pairs with A from the DNA sequence.\n")
     rnaStart= time.time() # time.time() returns the number of seconds since 00:00:00 UTC Jan. 01, 1776
-    rnaSequence = input("Please enter the matching RNA sequence. Leave no spaces! Then press enter.\n")
+    rnaSequence = input("Please enter the matching RNA sequence. Leave no spaces! Then press enter.\n").upper()
     rnaStop = time.time()
     rnaTime = rnaStop - rnaStart
     return(rnaSequence, rnaTime)
@@ -37,5 +38,28 @@ def genRNA(dnaSquence: str) -> tuple:
     # Tuples are UNCHANGEABLE -- you cannot add, modify, or delete after creating
     # Tuples CAN have duplicate values.
 
-rna = genRNA(dna)
-print(rna)
+def verifySequence(dnaSequence: str, rnaSequence: str) -> bool:
+    isMatch = False
+    if len(dnaSequence) != len(rnaSequence):
+        print("The sequences are different lengths and cannot match.\n")
+        return isMatch
+    for dnaBase, rnaBase in zip(dnaSequence, rnaSequence):
+         if dnaBase == "A" and rnaBase == "U":
+            isMatch = True
+         elif dnaBase == "C" and rnaBase == "G":
+            isMatch = True
+         elif dnaBase == "G" and rnaBase == "C":
+            isMatch =True
+         elif dnaBase == "T" and rnaBase == "A":
+            isMatch = True
+         else:
+            print ("Unable to identify correct base. No match detected.")
+    return isMatch
+
+
+
+dna= genDNA()
+rna = doTranscription(dna)
+print(verifySequence(dna, rna[0]))
+
+
