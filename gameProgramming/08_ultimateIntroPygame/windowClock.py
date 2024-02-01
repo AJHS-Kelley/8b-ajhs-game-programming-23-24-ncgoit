66f35b4e03e3d8c466919a44f1aaeea67a149f80
Nevaeh <111375939+ncgoit@users.noreply.github.com>
2024-02-01 14:33:52 -0500

    finished updating img's

--- gameProgramming/08_ultimateIntroPygame/windowClock.py
+++ gameProgramming/08_ultimateIntroPygame/windowClock.py
@@ -4,9 +4,12 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
-clock= pygame.time.Clock()
+clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
 
-test_surface = pygame.image.load('')
+sky_surface = pygame.image.load('img/ultimatePygame/skypicture.png')
+ground_surface = pygame.image.load('img/ultimatePygame/ground_surface.jpg')
+text_surface = test_font.render('My Game', False, 'Green')
 
 while True:
     for event in pygame.event.get():
@@ -14,7 +17,9 @@ while True:
             pygame.quit()
             exit()
     
-    screen.blit(test_surface,(200,100))
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
 
     pygame.display.update()
     clock.tick(60)
