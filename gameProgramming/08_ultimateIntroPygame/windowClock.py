9d556534627a59eaa81e9a96904f51a110bd4051
Nevaeh <111375939+ncgoit@users.noreply.github.com>
2024-02-05 14:10:00 -0500

    update

--- gameProgramming/08_ultimateIntroPygame/windowClock.py
+++ gameProgramming/08_ultimateIntroPygame/windowClock.py
@@ -11,15 +11,22 @@ sky_surface = pygame.image.load('img/ultimatePygame/skypicture.png')
 ground_surface = pygame.image.load('img/ultimatePygame/ground_surface.jpg')
 text_surface = test_font.render('My Game', False, 'Green')
 
+snail_surface = pygame.image.load('img/ultimatePygame/snailPicture.jfif')
+snail_x_pos = 600
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     
-    screen.blit(sky_surface,(0,0))
-    screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(300,50))
+    # screen.blit(sky_surface,(0,0))
+    # screen.blit(ground_surface,(0,300))
+    # screen.blit(text_surface,(300,50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100: snail_x_pos = 800
+    screen.blit(snail_surface,(snail_x_pos,250))
 
     pygame.display.update()
     clock.tick(60)
+
