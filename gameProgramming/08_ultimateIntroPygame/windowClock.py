6af44f2a9f469209dd3df79450476b8aa60feb8c
Nevaeh <s11944972@students.duvalschools.org>
2024-02-08 19:07:31 -0500

    player now able to jump

--- gameProgramming/08_ultimateIntroPygame/windowClock.py
+++ gameProgramming/08_ultimateIntroPygame/windowClock.py
@@ -18,14 +18,21 @@ snail_rect = snail_surface.get_rect(bottomright = (600, 300))
 
 player_surf = pygame.image.load('img/ultimatePygame/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rect.collidepoint(event.pos): print('collision')
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos):
+                player_gravity = -20
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player_gravity = -20
+
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
@@ -36,14 +43,11 @@ while True:
     snail_rect.x -= 4
     if snail_rect.right <= 0: snail_rect.left = 800
     screen.blit(snail_surface,snail_rect)
+
+    # Player
+    player_gravity += 1
+    player_rect.y += player_gravity
     screen.blit(player_surf,player_rect)
-
-    # if player_rect.colliderect(snail_rect):
-    #     print('collision')
-
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print (pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
