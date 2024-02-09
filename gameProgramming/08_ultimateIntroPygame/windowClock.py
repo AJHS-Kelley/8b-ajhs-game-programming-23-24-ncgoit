ea16a2bc05d78d6180c159f4439327103a93b8c4
Nevaeh <s11944972@students.duvalschools.org>
2024-02-08 19:13:18 -0500

    player can stand and jump without falling

--- gameProgramming/08_ultimateIntroPygame/windowClock.py
+++ gameProgramming/08_ultimateIntroPygame/windowClock.py
@@ -47,8 +47,9 @@ while True:
     # Player
     player_gravity += 1
     player_rect.y += player_gravity
+    if player_rect.bottom >= 300: player_rect.bottom = 300
     screen.blit(player_surf,player_rect)
-
+    
     pygame.display.update()
     clock.tick(60)
 
