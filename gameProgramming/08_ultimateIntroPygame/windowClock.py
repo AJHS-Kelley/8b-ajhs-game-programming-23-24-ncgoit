b072b98756c37d7870feea277c1d2e29988c70ea
Nevaeh <s11944972@students.duvalschools.org>
2024-02-08 19:32:22 -0500

    Update windowClock.py

--- gameProgramming/08_ultimateIntroPygame/windowClock.py
+++ gameProgramming/08_ultimateIntroPygame/windowClock.py
@@ -38,6 +38,7 @@ while True:
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
+                snail_rect.left = 800
 
     if game_active:
         screen.blit(sky_surface,(0,0))
