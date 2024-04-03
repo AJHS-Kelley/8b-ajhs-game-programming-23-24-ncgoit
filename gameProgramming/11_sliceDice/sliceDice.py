0582d744da8405a6226f096e6f8338795d0f232a
Nevaeh <s11944972@students.duvalschools.org>
2024-04-03 14:06:15 -0400

    slice

---
+++ gameProgramming/11_sliceDice/sliceDice.py
@@ -0,0 +1,26 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+myString = "I really like music, it's very calming.\n"
+
+# print(alphabet[0])
+# print(alphabet[-1])
+
+# Slicing
+print(alphabet[0:4]) # Slice UP TO second index.
+print(myString[0:5])
+# alphabet [start:stop]
+
+# Slide from the Start
+print(alphabet[:7])
+print(myString[:14])
+
+# Slice to the End
+print(alphabet[12:])
+print(myString[6:])
+
+# Slice the whole %#$#! Thing
+print(alphabet[:])
+print(myString[:])
+
+# Negative Index Slicing
+print(alphabet[-4:-1])
+print(myString[-7:2])
