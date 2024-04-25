cf3a44b5e868742cfbe55825d2f0b0f0374fafa3
Nevaeh <s11944972@students.duvalschools.org>
2024-04-25 14:43:15 -0400

    updated code

--- gameProgramming/09_finalProject/finalProject.py
+++ gameProgramming/09_finalProject/finalProject.py
@@ -111,9 +111,9 @@ while True:
 
         # Reset the delay
         delay = 0.1
-
+        
         pen.clear()
-        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
 
     # Check for a collision with the food
     if head.distance(food) < 20:
@@ -129,29 +129,64 @@ while True:
         new_segment.color("grey")
         new_segment.penup()
         segments.append(new_segment)
-
+    
         # Shorten the delay
         delay -= 0.001
 
-        # Increase the score
-        score += 10
 
+        # Increase the score
         if score > high_score:
             high_score = score
         
         pen.clear()
         pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
 
+
     # Move the end segments first in reverse order
     for index in range(len(segments)-1, 0, -1):
         x = segments[index-1].xcor()
         y = segments[index-1].ycor()
-        segments[index].goto(x, y)
+        segments[index].goto(x, y) 
+    
 
-    # Move segment 0 to where the head is
+        # Move segment 0 to where the head is
     if len(segments) > 0:
         x = head.xcor()
         y = head.ycor()
         segments[0].goto(x,y)
 
-    move()  
+
+    move()
+
+
+        # Check for head collision with the body segments
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+
+            # Clear the segments list
+            segments.clear()
+
+
+            # Reset the score
+            score = 0
+
+
+            # Reset the delay
+            delay = 0.1
+
+             # Update the score display
+            pen.clear()
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+
+    time.sleep(delay)
+
+
+wn.mainloop()
